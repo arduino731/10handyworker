e939d9ec5a23db913dcd5b10ae104a9a7dbfe889
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-03-16 21:04:52 -0500

    add tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    'gatsby-plugin-postcss',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
