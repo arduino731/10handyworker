b6d77f23b9ff2098fc2ce225432f5b44041e88e2
texas2010 <9701609+texas2010@users.noreply.github.com>
2022-03-16 22:20:41 -0500

    add logo in the index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
 
 import '../css/index.css';
+import logo from '../images/10HANDY.jpg';
 
 // markup
 const IndexPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div id='logo' className='flex justify-center'>
+        <img src={logo} alt='' className='w-64' />
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
